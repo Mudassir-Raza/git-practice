434e348dafaf7c229b92b7a3ccecd3abe6a9e9df
Mudassir Raza <razamudassir53@gmail.com>
2023-07-05 10:43:25 +0530

    Demo files for practice Initialize

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+var content = document.getElementById("content");
+
+btn.addEventListener("click", function() {
+  content.style.backgroundColor = "#ff0000";
+});
